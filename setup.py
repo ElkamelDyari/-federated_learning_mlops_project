38ca656bb7a7304c730e669d268c9f2064ba339e
Dyari Elkamel <dyarielkamel@gmail.com>
2024-11-25 23:49:37 +0100

    Add cookiecutter' template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Fedrated learning based Intrusion Detection System IDS with MLOps integration',
+    author='DyariElkamel',
+    license='MIT',
+)
